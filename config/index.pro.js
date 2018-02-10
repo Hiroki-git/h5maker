71dc85a6d0763b967d3bc82fe5fdd3266401da73
Hiroki <hiroki.zhu@fairlinkcentury.com>
2018-02-10 19:36:25 +0800

    调整配置

--- config/index.pro.js
+++ config/index.pro.js
@@ -5,7 +5,7 @@ const all = {
         session: 'h5maker'
     },
     mongo: {
-        uri: 'mongodb://192.168.32.45:27017/h5maker',
+        uri: 'mongodb://127.0.0.1:27017/h5maker',
         user: 'h5maker',
         pass: 'xgd$MPB37@8GALX#'
     },
